8d1565e3f0918e709a2ec0407ee1a5d9960406af
zeeshanwaris <zeeshan2424@gmail.com>
2025-02-18 11:21:44 -0800

    Eureka Server changes added in Section 8

---
+++ section8/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,33 @@
+package com.eazybytes.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+
+    @Schema(
+            description = "Account Number of Eazy Bank account", example = "3454433243"
+    )
+    @NotEmpty(message = "Account number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
+    private Long accountNumber;
+
+    @Schema(
+            description = "Account type of Eazy Bank account", example = "Savings"
+    )
+    @NotEmpty(message = "Account type cannot be empty")
+    private String accountType;
+
+    @Schema(
+            description = "Eazy Bank branch address", example = "123 NewYork"
+    )
+    @NotEmpty(message = "Branch address cannot be empty")
+    private String branchAddress;
+}

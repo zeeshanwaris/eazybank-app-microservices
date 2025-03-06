4afefa3285db6f63fe90ad294364fc38c2a41385
zeeshanwaris <zeeshan2424@gmail.com>
2025-03-06 01:26:46 -0800

    Added suppoert for Rabbit MQ and Spring Cloud Functions and Streams

---
+++ section13/accounts/src/main/java/com/eazybytes/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware {
+
+    /**
+     * @return
+     */
+    @Override
+    public Optional getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS");
+    }
+}
